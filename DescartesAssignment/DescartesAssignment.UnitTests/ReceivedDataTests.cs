d41e9eb220aea8c4b21632aeca7db75a27505c69
kosanovic.nikola <kosanovicnikola@yahoo.com>
2024-03-03 21:37:56 +0100

    New unit tests added

---
+++ DescartesAssignment/DescartesAssignment.UnitTests/ReceivedDataTests.cs
@@ -0,0 +1,30 @@
+﻿using DescartesAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescartesAssignment.UnitTests
+{
+    public class ReceivedDataTests
+    {
+        [Theory]
+        [InlineData("AA=", false)]
+        [InlineData("ABA=", true)]
+        [InlineData("AA@BDA==", false)]
+        [InlineData("ABAQAB==", true)]
+        [InlineData("AATBad?=", false)]
+        [InlineData("A/==", true)]
+        [InlineData("AA$BDA=D", false)]
+        public void IsReceivedDataValid(string data, bool expectedResult)
+        {
+            var receivedData = new ReceivedData();
+            {
+                receivedData.Data = data;
+            }
+            
+            Assert.Equal(receivedData.IsValid(), expectedResult);
+        }
+    }
+}
