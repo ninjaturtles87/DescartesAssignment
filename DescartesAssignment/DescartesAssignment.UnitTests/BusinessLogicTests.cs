3da975896f4c866ffa2e8d07f0bfc586dfe1b190
kosanovic.nikola <kosanovicnikola@yahoo.com>
2024-03-03 01:18:49 +0100

    Added DiferentiatingHelper unit test and refactored BusinessLogicTests

--- DescartesAssignment/DescartesAssignment.UnitTests/BusinessLogicTests.cs
+++ DescartesAssignment/DescartesAssignment.UnitTests/BusinessLogicTests.cs
@@ -1,5 +1,6 @@
 using DataLayer;
 using DataLayer.Models;
+using DescartesAssignment.Models;
 using DescartesAssignment.Services;
 using Moq;
 using System.Reflection;
@@ -10,56 +11,92 @@ namespace DescartesAssignment.UnitTests
     public class BusinessLogicTests
     {
         private readonly Mock<IDataAccess> _dataAccessMock;
+        private readonly BusinessLogic _businessLogic;
+
         public BusinessLogicTests()
         {
             _dataAccessMock = new Mock<IDataAccess>();
-        }
-        [Fact]
-        public async Task GetDifferences_SizeDoNotMatch_ReturnsCorrectResult()
-        {
-            var businessLogic = new BusinessLogic(_dataAccessMock.Object);
-            byte[] firstArray = new byte[] { 1, 2, 3 };
-            byte[] secondArray = new byte[] { 1, 2, 3, 4 };
-
-            var result = await businessLogic.GetDifferences(firstArray, secondArray);
-
-            Assert.Equal(DifferenceTypeEnums.SizeDoNotMatch.ToString(), result.DiffResultType);
+            _businessLogic = new BusinessLogic(_dataAccessMock.Object);
         }
 
         [Fact]
-        public async Task GetDifferences_Equals_ReturnsCorrectResult()
+        public async Task GetDifferences_WhenElementsExist_ReturnsOkDifferenceResponseProperties()
         {
-            var businessLogic = new BusinessLogic(_dataAccessMock.Object);
-            byte[] firstArray = new byte[] { 1, 2, 3 };
-            byte[] secondArray = new byte[] { 1, 2, 3 };
+            int id = 1;
+            var dataForComparisonList = new List<DataForComparison>
+            {
+                new DataForComparison
+                {
+                    Id = id, Side = DataSide.Left.ToString().ToLower(), Data = new byte[] { 1, 2, 3 }
+                },
+                new DataForComparison
+                {
+                    Id = id, Side = DataSide.Right.ToString().ToLower(), Data = new byte[] { 1, 2, 4 }
+                }
+            };
+            _dataAccessMock.Setup(x => x.GetDataByIdAsync(id)).ReturnsAsync(dataForComparisonList);
 
-            var result = await businessLogic.GetDifferences(firstArray, secondArray);
+            var result = await _businessLogic.GetDifferences(id);
 
-            Assert.Equal(DifferenceTypeEnums.Equals.ToString(), result.DiffResultType);
-        }
-
-        [Fact]
-        public async Task GetDifferences_ContentDoNotMatch_ReturnsCorrectResult()
-        {
-            var businessLogic = new BusinessLogic(_dataAccessMock.Object);
-            byte[] firstArray = new byte[] { 1, 2, 3 };
-            byte[] secondArray = new byte[] { 1, 2, 4 };
-
-            var result = await businessLogic.GetDifferences(firstArray, secondArray);
-
+            Assert.True(result.HasData);
             Assert.Equal(DifferenceTypeEnums.ContentDoNotMatch.ToString(), result.DiffResultType);
+            Assert.NotNull(result.Diffs);
             Assert.Equal(result.Diffs[0].Length, 1);
             Assert.Equal(result.Diffs[0].Offset, 2);
+        }
+
+        [Theory]
+        [InlineData(DataSide.Left)]
+        [InlineData(DataSide.Right)]
+        public async Task GetDifferences_WhenOneElementMissing_ReturnsDifferenceResponsePropertiesWithNoData(DataSide missingSide)
+        {
+            int id = 1;
+            var dataForComparisonList = new List<DataForComparison>();
+            if (missingSide == DataSide.Left)
+            {
+                dataForComparisonList.Add(
+                    new DataForComparison
+                    {
+                        Id = id,
+                        Side = DataSide.Right.ToString().ToLower(),
+                        Data = new byte[] { 1, 2, 4 }
+                    });
+            }
+            else
+            {
+                dataForComparisonList.Add(
+                    new DataForComparison
+                    {
+                        Id = id,
+                        Side = DataSide.Left.ToString().ToLower(),
+                        Data = new byte[] { 1, 2, 3 }
+                    });
+            }
+            _dataAccessMock.Setup(x => x.GetDataByIdAsync(id)).ReturnsAsync(dataForComparisonList);
+
+            var result = await _businessLogic.GetDifferences(id);
+
+            Assert.False(result.HasData);
+        }
+
+        [Fact]
+        public async Task GetDifferences_WhenBothElementsMissing_ReturnsDifferenceResponsePropertiesWithNoData()
+        {
+            int id = 1;
+            var dataForComparisonList = new List<DataForComparison>();
+            _dataAccessMock.Setup(x => x.GetDataByIdAsync(id)).ReturnsAsync(dataForComparisonList);
+
+            var result = await _businessLogic.GetDifferences(id);
+
+            Assert.False(result.HasData);
         }
 
         [Fact]
         public async Task PutValuesToDb_CallsSaveOrUpdateAsync()
         {
-            
-            var businessLogic = new BusinessLogic(_dataAccessMock.Object);
             var dataForComparison = new DataForComparison();
 
-            await businessLogic.PutValuesToDb(dataForComparison);
+            await _businessLogic.PutValuesToDb(dataForComparison);
 
             _dataAccessMock.Verify(x => x.SaveOrUpdateAsync(dataForComparison), Times.Once);
         }
---
+++ DescartesAssignment/DescartesAssignment.UnitTests/DifferentiatingHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using DescartesAssignment.Helpers;
+using DescartesAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescartesAssignment.UnitTests
+{
+    public class DifferentiatingHelperTests
+    {
+        [Fact]
+        public void GetDifferencesSpecifications_EqualArrays_ReturnsEmptyList()
+        {
+            byte[] firstArray = new byte[] { 1, 2, 3 };
+            byte[] secondArray = new byte[] { 1, 2, 3 };
+
+            List<DifferencesSpecified> result = DifferentiatingHelper.GetDifferencesSpecifications(firstArray, secondArray);
+
+            Assert.Empty(result);
+        }
+
+        [Theory]
+        [InlineData(new byte[] { 1, 2, 3 }, new byte[] { 1, 2, 4 }, 1, 2)]
+        [InlineData(new byte[] { 9, 8, 1, 2, 3 }, new byte[] { 1, 2, 3, 4, 5 }, 5, 0)]
+        public void GetDifferencesSpecifications_ContentDoNotMatch_ReturnsDifferencesSpecified(byte[] firstArray, byte[] secondArray, int length, int offset)
+        {
+            List<DifferencesSpecified> result = DifferentiatingHelper.GetDifferencesSpecifications(firstArray, secondArray);
+
+            Assert.Equal(result[0].Length, length);
+            Assert.Equal(result[0].Offset, offset);
+        }
+    }
+}
